acaeef2110d3a4490a432c81e0fb9df7c52b3cbd
lliurex <juanma1980@gmail.com>
2025-03-04 09:12:27 +0100

    customize qcheckable

--- src/stacks/cmbBtn.py
+++ src/stacks/cmbBtn.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import os
 import json
-from PySide2.QtWidgets import QLabel, QPushButton,QHBoxLayout,QGraphicsDropShadowEffect,QSizePolicy,QComboBox,QWidget
 from PySide2.QtCore import Qt,Signal
-from PySide2.QtGui import QIcon,QCursor,QMouseEvent,QPixmap,QImage,QPalette,QColor
-from QtExtraWidgets import QScreenShotContainer
+from QtExtraWidgets import QCheckableComboBox
 import gettext
 gettext.textdomain('appsedu')
 _ = gettext.gettext
@@ -12,26 +10,39 @@ _ = gettext.gettext
 i18n={"INSTALL":_("Install")}
 
 LAYOUT="appsedu"
-class QComboButton(QComboBox):
-	clicked=Signal("PyObject","PyObject")
+class QComboButton(QCheckableComboBox):
+	cmbClicked=Signal("PyObject","PyObject")
+	installClicked=Signal("PyObject","PyObject")
 	def __init__(self,parent=None,**kwargs):
-		QComboBox.__init__(self, parent)
+		QCheckableComboBox.__init__(self, parent)
 		self.showPop=False
-		self.overrideHide=QComboBox.hidePopup
+		self.setCursor(Qt.PointingHandCursor)
+		self.overrideHide=QCheckableComboBox.hidePopup
 		self.setEditable(True)
 		self.lineEdit().setDisabled(True)
 		self.lineEdit().setReadOnly(True)
 		self.lineEdit().setAlignment(Qt.AlignCenter)
+		self.title=False
+		self.exclusive=True
+		self.clicked.connect(self.hidePopup)
 	#def __init__
 
 	def setText(self,*args,**kwargs):
-		pass
+		self.lineEdit().setText(args[0])
 	#def setText
 
 	def mousePressEvent(self,e):
 		if (self.width()-e.x())>32:
 			e.accept()
-			self.clicked.emit(self.currentText().strip(),"")
+			sw=False
+			items=self.getItems()
+			for item in items:
+				if item.checkState() == Qt.Checked:
+					self.installClicked.emit(item.text().strip(),"")
+					sw=True
+					break
+			if sw==False:
+				self.installClicked.emit(items[0].text().strip(),"")
 			return(True)
 		else:
 			self.showPop=True
@@ -45,7 +56,19 @@ class QComboButton(QComboBox):
 			self.showPop=False
 		else:
 			self.overrideHide(self)
+			self.setText(i18n["INSTALL"].upper())
 		return(True)
 	#def hidePopup
 
+	def currentText(self):
+		text=""
+		items=self.getItems()
+		if len(items)>0:
+			for item in items:
+				if item.checkState() == Qt.Checked:
+					text=item.text().strip()
+					break
+			if text=="":
+				text=items[0].text().strip()
+		return(text)
 #class QComboButton
