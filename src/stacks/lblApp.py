eacd0ec8c746ee3f037ad4f3405ec67ba71a2d70
lliurex <juanma1980@gmail.com>
2025-03-18 10:00:23 +0100

    wip on split custom qwidgets

---
+++ src/stacks/lblApp.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+import os
+import json
+from PySide2.QtWidgets import QLabel
+from PySide2.QtCore import Qt,Signal
+from PySide2 import QtGui
+import gettext
+gettext.textdomain('appsedu')
+_ = gettext.gettext
+
+i18n={"INSTALL":_("Install")}
+ICON_SIZE=72
+
+LAYOUT="appsedu"
+class QLabelRebostApp(QLabel):
+	clicked=Signal("PyObject")
+	def __init__(self,parent=None):
+		QLabel.__init__(self, parent)
+		self.setAlignment(Qt.AlignCenter)
+		self.cacheDir=os.path.join(os.environ.get('HOME'),".cache","rebost","imgs")
+	#def __init__
+
+	def loadImg(self,app):
+		img=app.get('icon','')
+		self.setMinimumWidth(1)
+		icn=''
+		if os.path.isfile(img):
+			icn=QtGui.QPixmap.fromImage(QtGui.QImage(img))
+		elif img=='':
+			icn2=QtGui.QIcon.fromTheme(app.get('pkgname'),QtGui.QIcon.fromTheme("appedu-generic"))
+			icn=icn2.pixmap(ICON_SIZE,ICON_SIZE)
+		if icn:
+			wsize=ICON_SIZE
+			if "/usr/share/banners/lliurex-neu" in img:
+				wsize=int(ICON_SIZE*1.8)
+			self.setPixmap(icn.scaled(wsize,ICON_SIZE,Qt.KeepAspectRatio,Qt.SmoothTransformation))
+			self.setMinimumWidth(wsize+10)
+		elif img.startswith('http'):
+			aux=QScreenShotContainer()
+			self.scr=aux.loadScreenShot(img,self.cacheDir)
+			self.scr.start()
+			self.scr.imageLoaded.connect(self.load)
+			self.scr.wait()
+	#def loadImg
+	
+	def load(self,*args):
+		img=args[0]
+		self.setPixmap(img.scaled(ICON_SIZE,ICON_SIZE))
+		self.setMinimumWidth(ICON_SIZE+10)
+	#def load
+#class QLabelRebostApp
+
---
+++ src/stacks/lblLnk.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import os
+import json
+from PySide2.QtWidgets import QLabel,QWidget,QHBoxLayout
+from PySide2.QtCore import Qt,Signal
+from PySide2 import QtGui
+import gettext
+gettext.textdomain('appsedu')
+_ = gettext.gettext
+
+RSRC=os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))),"rsrc")
+
+LAYOUT="appsedu"
+class QLabelLink(QWidget):
+	def __init__(self,*args,**kwargs):
+		super().__init__()
+		hbox=QHBoxLayout()
+		icn=QtGui.QPixmap(os.path.join(RSRC,"link24x24.png"))
+		lblIcn=QLabel()
+		lblIcn.setPixmap(icn.scaled(16,16))
+		hbox.addWidget(lblIcn)
+		self.lbl=QLabel(args[0])
+		hbox.addWidget(self.lbl)
+		self.setLayout(hbox)
+	
+	def setOpenExternalLinks(self,*args):
+		self.lbl.setOpenExternalLinks(*args)
+
+	def setText(self,*args):
+		self.lbl.setText(*args)
+#class QLabelLink
+
