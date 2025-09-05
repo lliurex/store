2eec4fb15060fd05957f013aa593e7c435331ea5
lliurex <juanma1980@gmail.com>
2025-09-05 17:31:30 +0200

    smaller icon

--- src/stacks/lblLnk.py
+++ src/stacks/lblLnk.py
@@ -13,19 +13,23 @@ class QLabelLink(QWidget):
 	def __init__(self,*args,**kwargs):
 		super().__init__()
 		hbox=QHBoxLayout()
-		hbox.setSpacing(int(MARGIN))
+		hbox.setSpacing(0)
 		icn=QtGui.QPixmap(os.path.join(RSRC,"link24x24.png"))
 		lblIcn=QLabel()
-		lblIcn.setPixmap(icn.scaled(16,16,Qt.AspectRatioMode.KeepAspectRatio,Qt.TransformationMode.SmoothTransformation))
+		lblIcn.setPixmap(icn.scaled(12,12,Qt.AspectRatioMode.KeepAspectRatio,Qt.TransformationMode.SmoothTransformation))
+		lblIcn.setMinimumWidth(16)
 		hbox.addWidget(lblIcn)
 		self.lbl=QLabel(args[0])
 		hbox.addWidget(self.lbl,Qt.AlignLeft)
 		self.setLayout(hbox)
+	#def __init__
 	
 	def setOpenExternalLinks(self,*args):
 		self.lbl.setOpenExternalLinks(*args)
+	#def setOpenExternalLinks
 
 	def setText(self,*args):
 		self.lbl.setText(*args)
+	#def setText
 #class QLabelLink
 
