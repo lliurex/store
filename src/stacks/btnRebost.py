6780601767e1fe478be9e6bf997b95161eb4b633
lliurex <juanma1980@gmail.com>
2025-09-19 10:05:00 +0200

    force minimum size in compact mode

--- src/stacks/btnRebost.py
+++ src/stacks/btnRebost.py
@@ -125,7 +125,7 @@ class QPushButtonRebostApp(QPushButton):
 		if args[0]==True:
 			self._showBtn=False
 			self._compactMode=True
-			self.setFixedSize(QSize(ICON_SIZE*1.6,ICON_SIZE*1.2))
+			self.setMinimumSize(QSize(ICON_SIZE*1.5,ICON_SIZE*1.3))
 			self.appIconSize=(ICON_SIZE*0.4)
 	#def setCompactMode
 
