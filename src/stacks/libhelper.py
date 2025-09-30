3087da17b906290e853ac2501a0514efc033c7ad
lliurex <juanma1980@gmail.com>
2025-09-30 11:02:40 +0200

    Better way for find related zmd

--- src/stacks/libhelper.py
+++ src/stacks/libhelper.py
@@ -32,78 +32,47 @@ class helper():
 	def runZmd(self,app):
 		ret=-1
 		cmd=[]
-		zmdCmd=app.get('bundle',{}).get('unknown','').replace(".epi","")+".zmd"
+		zmdCmd=app.get('bundle',{}).get('unknown','')
 		appName=app.get("pkgname","")
 		if appName=="":
 			appName=zmdCmd
-		if zmdCmd.endswith(".zmd")==True:
-			zmdPath=os.path.join("/usr/share/zero-center/zmds",zmdCmd)
-			if os.path.exists(zmdPath)==False:
-				alternatives=["zero-lliurex-{}".format(zmdCmd),"zero-installer-{}".format(zmdCmd),"zero-fp-{}".format(zmdCmd)]
-				for f in os.scandir(os.path.dirname(zmdPath)):
-					if f.name in alternatives:
-						zmdPath=f.path
+		#Patch for zero-lliurex-adobereader
+		if zmdCmd=="acroread.epi":
+			zmdCmd="zero-lliurex-adobereader.zmd"
+		if zmdCmd.endswith(".zmd")==zmdCmd.endswith(".epi")==False:
+			zmdCmd+=".zmd"
+		zmdCmd=zmdCmd.replace(".epi",".zmd")
+		zmdPath=os.path.join("/usr/share/zero-center/zmds",zmdCmd)
+		if os.path.exists(zmdPath)==False:
+			alternatives=["zero-lliurex-{}".format(zmdCmd),"zero-installer-{}".format(zmdCmd),"zero-fp-{}".format(zmdCmd)]
+			for f in os.scandir(os.path.dirname(zmdPath)):
+				if f.name in alternatives:
+					zmdPath=f.path
+					break
+		if os.path.exists(zmdPath):
+			cmd=self._getCmdFromZmd(zmdPath)
+			#subprocess.run(["pkexec",zmdPath])
+			try:
+				cmd.append(appName)
+				proc=subprocess.run(cmd)
+				ret=proc.returncode
+			except Exception as e:
+				print(e)
+				ret=-1
+			if ret>0:
+				#Zmd could depend on a zmd-installer so let's search
+				zmdFolder=os.path.dirname(zmdPath)
+				searchZmd=".".join(zmdPath.split(".")[:-1])
+				newPath=zmdPath
+				for f in os.scandir(zmdFolder):
+					if searchZmd in f.path and f.path!=zmdPath:
+						newPath=f.path
 						break
-			if os.path.exists(zmdPath):
-				cmd=self._getCmdFromZmd(zmdPath)
-				#subprocess.run(["pkexec",zmdPath])
-				try:
-					cmd.append(appName)
-					proc=subprocess.run(cmd)
-					ret=proc.returncode
-				except Exception as e:
-					print(e)
-					ret=-1
-				if ret>0:
-					#Zmd could depend on a zmd-installer so let's search
-					zmdFolder=os.path.dirname(zmdPath)
-					searchZmd=".".join(zmdPath.split(".")[:-1])
-					newPath=zmdPath
-					for f in os.scandir(zmdFolder):
-						if searchZmd in f.path and f.path!=zmdPath:
-							newPath=f.path
-							break
-					if zmdPath!=newPath:
-						cmd=self._getCmdFromZmd(newPath)
-						#subprocess.run(["pkexec",zmdPath])
-		elif zmdCmd.endswith(".epi"):
-			cmd=["pkexec","epi-gtk",zmdCmd,appName]
-			if len(cmd)>0:
-				try:
-					proc=subprocess.run(cmd,env=os.environ)
-				except Exception as e:
-					print(e)
-					ret=-1
-
-				if ret>=0:
-					ret=proc.returncode
-			else:
-				self._zmdNotFound(zmdPath)
+				if zmdPath!=newPath:
+					cmd=self._getCmdFromZmd(newPath)
+					#subprocess.run(["pkexec",zmdPath])
 		return(ret)
 	#def runZmd
-
-	def _zmdNotFound(self,zmd):
-	#	def _launchZeroCenter():
-	#		dlg.close()
-		cmd=["zero-center"]
-		try:
-			subprocess.run(cmd)
-		except Exception as e:
-			print(e)
-
-	#	dlg=QDialog()
-	#	dlg.setWindowTitle("Error")
-	#	btns=QDialogButtonBox.Open|QDialogButtonBox.Cancel
-	#	dlgBtn=QDialogButtonBox(btns)
-	#	dlgBtn.accepted.connect(_launchZeroCenter)
-	#	dlgBtn.rejected.connect(dlg.close)
-	#	lay=QGridLayout()
-	#	lbl=QLabel("{0}".format(i18n.get("ZMDNOTFOUND")))
-	#	lay.addWidget(lbl)
-	#	lay.addWidget(dlgBtn)
-	#	dlg.setLayout(lay)
-	#	dlg.exec()
-	#def _zmdNotFound(self,zmd):
 
 	def getLauncherForBundle(self,app,bundle):
 		launchers={"flatpak":["flatpak","run"],"snap":["snap","run"]}
