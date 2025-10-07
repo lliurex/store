a45200039afbbc9d5fa7730aa33b3238f9904df8
lliurex <juanma1980@gmail.com>
2025-10-07 08:26:00 +0200

    Embed qthread for images

--- src/stacks/lblApp.py
+++ src/stacks/lblApp.py
@@ -2,8 +2,9 @@
 import os
 from functools import partial
 import json
+import requests
 from PySide2.QtWidgets import QLabel
-from PySide2.QtCore import Qt,Signal,QSize
+from PySide2.QtCore import Qt,Signal,QSize,QThread
 from PySide2 import QtGui
 from QtExtraWidgets import QScreenShotContainer
 import gettext
@@ -13,13 +14,73 @@ _ = gettext.gettext
 
 i18n={"INSTALL":_("Install")}
 
+class _imageLoader(QThread):
+	fetched=Signal("PyObject")
+	def __init__(self,*args,**kwargs):
+		super().__init__()
+		self.dbg=True
+		self.uri=kwargs.get("uri","")
+		self.pxm=kwargs.get("pxm","")
+		self.name=os.path.basename(self.uri)
+		self.pxm=QtGui.QPixmap()
+		self.cacheDir=os.path.join(os.environ.get('HOME'),".cache","rebost","imgs")
+	#def __init__
+	
+	def _debug(self,msg):
+		if self.dbg==True:
+			print("_imageLoader: {}".format(msg))
+	#def _debug
+
+	def setPxm(self,pxm):
+		self.pxm=pxm
+	#def setPxm
+
+	def setUri(self,uri):
+		self.uri=uri
+	#def setUri
+
+	def run(self):
+		pxm=self.pxm
+		if pxm.isNull():
+			uri=self.uri
+			if self.uri!="":
+				if not os.path.exists(uri):
+					uri=os.path.join(self.cacheDir,os.path.basename(uri))
+				if not os.path.exists(uri):
+					uri=self.uri
+			if not os.path.exists(uri):
+				if uri==os.path.basename(uri):
+					icn=QtGui.QIcon.fromTheme(uri)
+					if icn.isNull():
+						icn=QtGui.QIcon.fromTheme("appedu-generic")
+					pxm=icn.pixmap(QSize(64,64))
+				elif "://":
+					try:
+						img=requests.get(uri,timeout=2)
+						img.close()
+						pxm.loadFromData(img.content)
+						if self.cacheDir:
+							fPath=os.path.join(self.cacheDir,os.path.basename(uri))
+							if not os.path.exists(fPath):
+								pxm=pxm.scaled(256,256,Qt.AspectRatioMode.KeepAspectRatio,Qt.TransformationMode.SmoothTransformation)
+								pxm.save(fPath,"PNG")#,quality=5)
+					except:
+						icn=QtGui.QIcon.fromTheme("appedu-generic")
+						pxm=icn.pixmap(QSize(64,64))
+			else:
+				pxm=QtGui.QPixmap()
+				pxm.load(uri)
+		self._debug("Emit {}".format(pxm))
+		self.fetched.emit(pxm)
+#class _imageLoader(QThread):
+
 class QLabelRebostApp(QLabel):
 	clicked=Signal("PyObject")
 	def __init__(self,parent=None):
 		QLabel.__init__(self, parent)
 		self.setAlignment(Qt.AlignCenter)
-		self.cacheDir=os.path.join(os.environ.get('HOME'),".cache","rebost","imgs")
-		self.scrCnt=QScreenShotContainer()
+		self._imageLoader=_imageLoader()
+		self._imageLoader.fetched.connect(self._setIcon)
 		self.iconSize=-1
 		self.pixmapPath=""
 		self.app={}
@@ -29,17 +90,32 @@ class QLabelRebostApp(QLabel):
 
 	@staticmethod
 	def _stop(*args):
-		self=args[0]
-		if "scr" in self.keys():
-			self["scr"].blockSignals(True)
-			self["scr"].requestInterruption()
-			self["scr"].deleteLater()
-			self["scr"].wait()
+		selfDict=args[0]
+		#if "scrCnt" in selfDict.keys():
+		#	selfDict["scrCnt"].blockSignals(True)
+		#	selfDict["scrCnt"].deleteLater()
+		if "th" in selfDict.keys():
+			for th in selfDict["th"]:
+				th.finished.emit()
+				th.blockSignals(True)
+				th.requestInterruption()
+				th.quit()
+				th.deleteLater()
+				th.wait()
 	#def _stop(*args):
 
 	def setIconSize(self,size):
 		self.iconSize=size
 	#def setIconSize
+
+	def _setIcon(self,*args):
+		if self.iconSize>0:
+			baseSize=self.iconSize
+		else:
+			baseSize=ICON_SIZE
+		wsize=baseSize
+		self.setPixmap(args[0].scaled(wsize,baseSize,Qt.IgnoreAspectRatio,Qt.FastTransformation))
+	#def _setIcon(self,*args):
 
 	def loadImg(self,app):
 		self.app=app
@@ -52,25 +128,12 @@ class QLabelRebostApp(QLabel):
 		self.pixmapPath=""
 		img=app.get('icon','')
 		if isinstance(img,QtGui.QPixmap):
-			self.load(img)
-			return
-		elif os.path.isfile(img):
-			self.pixmapPath=img
-		elif img=='':
-			icn=QtGui.QIcon.fromTheme(app.get('pkgname'),QtGui.QIcon.fromTheme("appedu-generic"))
-			if not icn.isNull():
-				self.setPixmap(icn.pixmap(QSize(wsize,baseSize)))
-				load=False
-		elif os.path.exists(os.path.join(self.cacheDir,os.path.basename(img))):
-			img=os.path.join(self.cacheDir,os.path.basename(img))
-			self.pixmapPath=img
-		if load:
-			scrCnt=QScreenShotContainer()
-			scr=scrCnt.loadScreenShot(img,self.cacheDir)
-			scr.imageReady.connect(self.load)
-			scr.start()
-			self.th.append(scr)
-	#	self.scr.wait()
+			self._imageLoader.setPxm(img)
+		elif not isinstance(img,str):
+			img=""
+		self._imageLoader.setUri(img)
+		self._imageLoader.start()
+		self.th.append(self._imageLoader)
 	#def loadImg
 
 	def load(self,*args,**kwargs):
