db08c9c047c740f91004024c77f86be4c01cd0ef
juanma <juanma1980@gmail.com>
2025-11-18 19:00:03 +0100

    Honour unavailable

--- src/stacks/btnRebost.py
+++ src/stacks/btnRebost.py
@@ -225,13 +225,13 @@ class QPushButtonRebostApp(QPushButton):
 	def _setActionForButton(self):
 		self.btn.setText(i18n["INSTALL"])
 		self.btn.setEnabled(True)
-		if self.app.get("forbidden",False)==True:
-			self.btn.setEnabled(False)
-			self.btn.setText(i18n["UNAUTHORIZED"])
-		elif len(self.app.get("bundle",[]))==0 or self.app.get("unavailable",False)==True:
+		if len(self.app.get("bundle",[]))==0 or self.app.get("unavailable",False)==True:
 			self.btn.setText(i18n.get("UNAVAILABLE"))
 			self.btn.setText(i18n["UNAVAILABLE"])
 			self.btn.setEnabled(False)
+		elif self.app.get("forbidden",False)==True:
+			self.btn.setEnabled(False)
+			self.btn.setText(i18n["UNAUTHORIZED"])
 		else: #app seems authorized and available
 			bundles=self.app["bundle"]
 			status=self.app["status"]
@@ -289,10 +289,10 @@ class QPushButtonRebostApp(QPushButton):
 			self._setActionForButton()
 		self.iconUri.setVisible(True)
 		self.flyIcon=""
-		if self.app.get("forbidden",False)==True:
-			self.flyIcon=QPixmap(os.path.join(RSRC,"appsedu_forbidden128x128.png"))
-		elif self.app.get("unavailable",False)==True:
+		if self.app.get("unavailable",False)==True:
 			self.flyIcon=QPixmap(os.path.join(RSRC,"appsedu_unavailable128x128.png"))
+		elif self.app.get("forbidden",False)==True:
+			self.flyIcon=QPixmap(os.path.join(RSRC,"appsedu_forbidden128x128.png"))
 		elif self.app.get("name","").startswith("zero-"):
 			self.flyIcon=QPixmap(os.path.join(RSRC,"zero-center128x128.png"))
 		elif self.app.get("homepage")!=None:
