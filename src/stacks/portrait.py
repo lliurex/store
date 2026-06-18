5fe7662bb7a7372726639295ddea9408c22a1130
lliurex <juanma1980@gmail.com>
2026-06-18 11:27:02 +0200

    Move llxup button to top

--- src/stacks/portrait.py
+++ src/stacks/portrait.py
@@ -6,10 +6,10 @@ import subprocess
 import json
 import dbus
 import dbus.mainloop.glib
-from PySide6.QtWidgets import QApplication, QLineEdit,QLabel,QPushButton,QGridLayout,QHBoxLayout, QWidget,QVBoxLayout,QListWidget, \
+from PySide2.QtWidgets import QApplication, QLineEdit,QLabel,QPushButton,QGridLayout,QHBoxLayout, QWidget,QVBoxLayout,QListWidget, \
 							QCheckBox,QListWidgetItem,QSizePolicy
-from PySide6 import QtGui
-from PySide6.QtCore import Qt,QSize,Signal,QThread,QEvent#,QTimer
+from PySide2 import QtGui
+from PySide2.QtCore import Qt,QSize,Signal,QThread,QEvent#,QTimer
 from QtExtraWidgets import QStackedWindowItem
 from rebost import store 
 from wdg.btnRebost import QPushButtonRebostApp
@@ -458,8 +458,6 @@ class portrait(QStackedWindowItem):
 		self.lstCategories.itemActivated.connect(self._loadCategory)
 		self.lstCategories.itemClicked.connect(self._loadCategory)
 		self.lstCategories.hide()
-		self.lblInfo=self._defInfo()
-		vbox.addWidget(self.lblInfo,Qt.Alignment(-1))
 		vbox.setStretch(0,0)
 		vbox.setStretch(1,2)
 		vbox.setStretch(3,1)
@@ -488,7 +486,9 @@ class portrait(QStackedWindowItem):
 		lbl.setClickable(True)
 		#lbl.setObjectName("banner")
 		img=os.path.join(RSRC,"banner.svg")
-		pxm=QtGui.QPixmap(img).scaled(172,64,Qt.KeepAspectRatio,Qt.SmoothTransformation)
+		x=int(QApplication.primaryScreen().size().width()/6)
+		y=int(x*9/16)
+		pxm=QtGui.QPixmap(img).scaled(x,y,Qt.KeepAspectRatio,Qt.SmoothTransformation)
 		lbl.setPixmap(pxm)
 		lbl.clicked.connect(self._goHome)
 		return lbl
@@ -503,6 +503,8 @@ class portrait(QStackedWindowItem):
 		self.sortAsc=False
 		banner=self._defBanner()
 		lay.addWidget(banner,0,0,1,1,Qt.AlignCenter|Qt.AlignTop)
+		self.lblInfo=self._defInfo()
+		lay.addWidget(self.lblInfo,0,0,1,1,Qt.AlignBottom)
 		_defBtnBar=self._defBtnBar()
 		#lay.addWidget(_defBtnBar,1,0,1,1,Qt.AlignCenter)
 		navBar=self._defNavigationBar()
