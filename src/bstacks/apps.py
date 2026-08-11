9b03210b082f2bd26c76ad4ff33516a27570bbb5
lliurex <juanma1980@gmail.com>
2026-08-12 00:40:55 +0200

    Signal install app. Clean exit

--- src/bstacks/apps.py
+++ src/bstacks/apps.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import json
+from functools import partial
 from PySide6.QtWidgets import QWidget,QGridLayout,QPushButton,QLabel,QHBoxLayout,QApplication
 from PySide6.QtCore import Qt,Signal
-from QtExtraWidgets import QSearchBox,QFlowTouchWidget,QPushInfoButton
+from QtExtraWidgets import QSearchBox,QFlowTouchWidget
 from extras.i18n import *
 from extras.constants import *
 from wdg import btnApp
@@ -11,12 +12,22 @@ from wdg import btnApp
 class QAppsPane(QWidget):
 	ready=Signal()
 	beginLoad=Signal("PyObject")
+	requestInstall=Signal("PyObject")
 	def __init__(self,*args,parent=None,**kwargs):
+		self.__EXIT__=False
 		QWidget.__init__(self, parent)
 		self.rebost=kwargs.get("rebost")
+		self.actionAppBtn={}
 		self.__initScreen__()
 		self.beginLoad.connect(self._loadGrid)
+		self.destroyed.connect(partial(QAppsPane._onDestroy,self.__dict__))
 	#def __init__
+
+	@staticmethod
+	def _onDestroy(*args):
+		selfDict=args[0]
+		selfDict["__EXIT__"]=True
+	#def _onDestroy
 
 	def _defSearch(self):
 		wdg=QSearchBox()
@@ -51,6 +62,13 @@ class QAppsPane(QWidget):
 		self.layout().setRowStretch(1,1)
 	#def __initScreen__
 
+	def _installApp(self,*args):
+		btn=self.flow.currentItem()
+		btn.setDisabled(True)
+		self.requestInstall.emit(btn)
+		self.actionAppBtn.update({btn.app["id"]:btn})
+	#def _installApp
+
 	def _loadGrid(self,apps):
 		wSize=(self.sizeHint().width()/2)+SPACING*4
 		if len(apps)>0:
@@ -60,17 +78,24 @@ class QAppsPane(QWidget):
 				if isinstance(app,str):
 					app=json.loads(app)
 				btn=btnApp.QAppButton(app)
-				self.flow.addWidget(btn)
-				QApplication.processEvents()
-				#btn.setMinimumWidth((self.width()+9*SPACING)/3)
 				btn.setFixedWidth(wSize)
-		self.ready.emit()
+				if self.__EXIT__==False:
+					self.flow.addWidget(btn)
+					btn.setMinimumWidth((self.width()+9*SPACING)/3)
+					btn.clicked.connect(self._installApp)
+				else:
+					break
+				QApplication.processEvents()
+			if self.__EXIT__==False:
+				self.ready.emit()
+	#def _loadGrid
 
 	def load(self,*args,category=False):
 		self.flow.clean()
 		if category==True:
 			apps=json.loads(self.rebost.getAppsInCategory(args[0]))
 			apps=apps[args[0]]
+			apps=sorted(apps, key=lambda x: x["name"].lower())
 		else:
 			apps=json.loads(self.rebost.searchApp(args[0]))
 		self.beginLoad.emit(apps)
