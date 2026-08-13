13adb4a350d7a3668d93814d2ae987f96c43b028
lliurex <juanma1980@gmail.com>
2026-08-13 13:42:39 +0200

    Improve selection of apps

--- src/bstacks/wdg/choice.py
+++ src/bstacks/wdg/choice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 from wdg.flowBar import QFlowBar
+from random import shuffle
 from lib import rss
 
 class choiBar(QFlowBar):
@@ -10,8 +11,8 @@ class choiBar(QFlowBar):
 		self.rss=rss.rssParser()
 		self.rss.choiceEnded.connect(self._loadData)
 		self.itemsPerPage=3
-		self.spacing=25
-		self.defaultSize=64
+		self.spacing=15
+		self.defaultSize=48
 	#def __init__(self,*args):
 
 	def loadChoice(self):
@@ -24,10 +25,30 @@ class choiBar(QFlowBar):
 
 	def _loadData(self,*args):
 		feed,content=args
+		appContent={}
 		for idx,data in content.items():
 			t=data.get("title")
 			app=json.loads(self.rebost.showApp(data["title"]))
 			if len(app)>0:
-				data.update({"summary":app[0]["description"]})
-		self.updateScreen(feed,content)
+				data.update({"summary":"","metadata":app[0]["id"],"img":app[0]["icon"]})
+				appContent[len(appContent)]=data
+		cont=0
+		apps=json.loads(self.rebost.searchApp("lliurex"))
+		shuffle(apps)
+		while cont<3:
+			for app in apps:
+				if app.get("forbidden",False)==True or app.get("unavailable",False)==True:
+					continue
+				if app["name"].startswith("zero-lliurex"):
+					continue
+				if len(app["name"])>20:
+					continue
+				appContent[len(appContent)]={"summary":"","metadata":app["id"],"img":app["icon"],"title":app["name"].strip()}
+				cont+=1
+		keys=list(appContent.keys())
+		shuffle(keys)
+		selectedContent={}
+		for i in keys[0:min(5,len(keys))]:
+			selectedContent[i]=appContent[i]
+		self.updateScreen(feed,selectedContent)
 	#def _loadData
