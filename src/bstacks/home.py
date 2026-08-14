e62067215f6ebd0d39290cdf0fb0883fc617092e
lliurex <juanma1980@gmail.com>
2026-08-14 10:32:45 +0200

    Add receipts section

--- src/bstacks/home.py
+++ src/bstacks/home.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QWidget,QGridLayout,QPushButton,QLabel
 from PySide6.QtCore import Qt,Signal
 from wdg.topBar import QTopBar
 from wdg.blog import blogBar
+from wdg.receipts import recsBar
 from wdg.choice import choiBar
 from wdg.categories import catsBar
 from wdg.zomandos import zmdsBar
@@ -42,6 +43,7 @@ class QHomePane(QWidget):
 				chld.setText(i18n.get(chld.property("name"),chld.property("name")).upper())
 		#wdg.loadHome.connect(self._goHome)
 		wdg.loadNews.connect(self._loadContent)
+		wdg.loadBlog.connect(self._loadContent)
 		wdg.loadRecs.connect(self._loadContent)
 		wdg.loadZmds.connect(self._loadContent)
 		wdg.loadCats.connect(self._loadContent)
@@ -50,14 +52,30 @@ class QHomePane(QWidget):
 
 	def _loadContent(self,content):
 		self.flowBlog.hide()
+		self.flowRecs.hide()
 		self.flowCats.hide()
 		self.flowZmds.hide()
 		if content=="news":
 			self.flowBlog.show()
 		elif content=="cats":
 			self.flowCats.show()
+		elif content=="recs":
+			self.flowRecs.show()
 		elif content=="zmds":
 			self.flowZmds.show()
+
+	def _emitLoadRec(self,*args):
+		txt=args[0].property("metadata")
+		href=txt.split("href=\"")[-1].split("\"")[0]
+		self.runner.setAction("xdg-open",href)
+		self.runner.start()
+	#def _emitLoadRec
+
+	def _defRecsBar(self):
+		wdg=recsBar()
+		wdg.selected.connect(self._emitLoadRec)
+		return(wdg)
+	#def _defRecsBar
 
 	def _emitLoadUrl(self,*args):
 		txt=args[0].lblDesc.text()
@@ -110,17 +128,21 @@ class QHomePane(QWidget):
 		lay.setSpacing(0)
 		topBar=self._topBar()
 		lay.addWidget(topBar,0,0,1,self.layout().columnCount(),Qt.AlignTop|Qt.AlignCenter)
-		self.flowBlog=self._defBlogBar()
-		self.flowBlog.loadBlog()
-		lay.addWidget(self.flowBlog,1,0,1,self.layout().columnCount(),Qt.AlignTop)
-		self.flowCats=self._defCatsBar()
-		self.flowCats.defaultSize=self.flowBlog.sizeHint().height()-SPACING
-		self.flowCats.loadCategories()
-		lay.addWidget(self.flowCats,1,0,1,self.layout().columnCount(),Qt.AlignTop)
 		self.flowZmds=self._defZmdsBar()
-		self.flowZmds.defaultSize=self.flowBlog.sizeHint().height()-SPACING
 		self.flowZmds.loadZomandos()
 		lay.addWidget(self.flowZmds,1,0,1,self.layout().columnCount(),Qt.AlignTop)
+		self.flowCats=self._defCatsBar()
+		self.flowCats.loadCategories()
+		self.flowCats.hide()
+		lay.addWidget(self.flowCats,1,0,1,self.layout().columnCount(),Qt.AlignTop)
+		self.flowRecs=self._defRecsBar()
+		self.flowRecs.loadRecs()
+		self.flowRecs.hide()
+		lay.addWidget(self.flowRecs,1,0,1,self.layout().columnCount(),Qt.AlignTop)
+		self.flowBlog=self._defBlogBar()
+		self.flowBlog.loadBlog()
+		self.flowBlog.hide()
+		lay.addWidget(self.flowBlog,1,0,1,self.layout().columnCount(),Qt.AlignTop)
 		searchBox=self._defSearch()
 		searchBox.setMinimumWidth(512)
 		#self.layout().addWidget(QLabel("<hr>"),2,0,1,self.layout().columnCount(),Qt.AlignTop|Qt.AlignCenter)
@@ -128,13 +150,12 @@ class QHomePane(QWidget):
 		self.layout().addWidget(QLabel("{}".format(i18n["CHOICE"])),3,0,1,self.layout().columnCount(),Qt.AlignBottom|Qt.AlignCenter)
 		self.flowChoi=self._defChoiBar()
 		self.flowChoi.loadChoice()
-		lay.addWidget(self.flowChoi,4,0,1,self.layout().columnCount())
+		lay.addWidget(self.flowChoi,4,0,1,self.layout().columnCount(),Qt.AlignTop)
 		self.layout().addWidget(QLabel("<hr>".format(i18n["CHOICE"])),5,0,1,self.layout().columnCount(),Qt.AlignTop|Qt.AlignCenter)
-		lay.setRowStretch(0,1)
-		lay.setRowStretch(1,0)
+		lay.setRowStretch(0,0)
+		lay.setRowStretch(1,1)
 		lay.setRowStretch(2,1)
-		lay.setRowStretch(3,1)
-		lay.setRowStretch(4,0)
-		lay.setRowStretch(5,1)
+		lay.setRowStretch(3,0)
+		lay.setRowStretch(4,1)
 	#def __initScreen__
 
