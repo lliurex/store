a9bfe1b4017af52604a2c9f19b6aae03437f3086
lliurex <juanma1980@gmail.com>
2026-08-18 17:54:40 +0200

    Thread load

--- src/bstacks/wdg/choice.py
+++ src/bstacks/wdg/choice.py
@@ -9,11 +9,13 @@ from extras.constants import *
 
 class choiBar(QFlowBar):
 	ready=Signal()
+	exception=Signal(str)
 	def __init__(self,*args,parent=None,**kwargs):
 		QFlowBar.__init__(self, parent)
 		self.rebost=kwargs.get("rebost")
 		self.rebostQuery=rebostQuery(rebost=self.rebost)
 		self.rebostQuery.queryCompleted.connect(self._processQueryResult)
+		self.rebostQuery.exception.connect(self._thException)
 		self.rss=rss.rssParser()
 		self.rss.choiceEnded.connect(self._beginLoadData)
 		self.itemsPerPage=3
@@ -23,6 +25,10 @@ class choiBar(QFlowBar):
 		self.loadImgSync=True
 		self.showScrollBar(True)
 	#def __init__
+
+	def _thException(self,*args):
+		self.exception.emit(*args)
+	#def _thException
 
 	def loadChoice(self):
 		self.rss.feed="lliurexnet"
@@ -35,30 +41,33 @@ class choiBar(QFlowBar):
 			if len(app)==0:
 				continue
 			t=app[0]["name"].strip()
-			self.content[t.replace(" ","")]={"summary":"","metadata":app[0]["id"],"img":app[0]["icon"],"title":t}
-		self.rebostQuery.setQuery("search","lliurex")
+			self.content[len(self.content)]={"summary":"","metadata":app[0]["id"],"img":app[0]["icon"],"title":t}
+		self.rebostQuery.setQuery("search","appsedu")
 		self.rebostQuery.start()
 	#def _loadAppData
 
 	def _endLoadData(self,*args):
 		apps=args[0]
 		shuffle(apps)
-		cont=0
-		while cont<3:
-			for app in apps:
-				if app.get("forbidden",False)==True or app.get("unavailable",False)==True:
-					continue
-				if app["name"].startswith("zero-lliurex"):
-					continue
-				if len(app["name"])>20:
-					continue
-				t=app["name"].strip()
-				self.content[t.replace(" ","")]={"summary":"","metadata":app["id"],"img":app["icon"],"title":t}
-				cont+=1
+		if apps==None:
+			return
+		apps=apps[0:min(5,len(apps))]
+		newApps={}
+		while len(apps)>0:
+			app=apps.pop()
+			t=app["name"].strip()
+			if app.get("forbidden",False)==True or app.get("unavailable",False)==True:
+				continue
+			if app["name"].startswith("zero-lliurex"):
+				continue
+			if len(app["name"])>20:
+				continue
+			newApps[len(newApps)+len(self.content)]={"summary":"","metadata":app["id"],"img":app["icon"],"title":t}
+		self.content.update(newApps)
 		keys=list(self.content.keys())
 		shuffle(keys)
 		selectedContent={}
-		for i in keys[0:min(5,len(keys))]:
+		for i in keys[0:min(10,len(keys))]:
 			selectedContent[i]=self.content[i]
 		bheight=self.defaultSize*2
 		self.table.setRowHeight(0,bheight-SPACING)
